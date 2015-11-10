6c841bec512f6794e76e90884877f7a46f1f834c
Hemanth A <hemanth42a@gmail.com>
2015-11-11 03:14:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='arduino_reddit',
       version='1',
-      description='Program to Check for new Reddit messages and output to LED on Raspberry Pi GPIO',
+      description='Program to Check for new Reddit messages and output to LED on Arduino',
       url='https://github.com/hemanth42/Arduino-Reddit',
       author='Hemanth A',      
       license='MIT',
